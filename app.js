db8b05df855603995bc66cc5b9b33cc28c8060cc
“swtpumpkin” <swtpumpkin90@gmail.com>
2020-03-24 14:44:39 +0900

    Create test code

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ const port = 8080;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app;
---
+++ test.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest');
+const expect = require('chai').expect;
+const app = require('./app');
+const agent = supertest.agent(app);
+
+
+describe('node docker test', () => {
+  it('get test', () => {
+    return agent.get('/').expect(200).then(res => expect(res.text).to.be.equal('Hello World!'));
+  });
+});
+
