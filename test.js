7c2156087e43166159d5de1e9eebb7d2bdd10085
“swtpumpkin” <swtpumpkin90@gmail.com>
2020-03-24 15:40:19 +0900

    Remove empty enter

--- test.js
+++ test.js
@@ -3,10 +3,8 @@ const expect = require('chai').expect;
 const app = require('./app');
 const agent = supertest.agent(app);
 
-
 describe('node docker test', () => {
   it('get test', () => {
     return agent.get('/').expect(200).then(res => expect(res.text).to.be.equal('Hello World!'));
   });
 });
-
